be07446841c4239b9b6340019195472341880ab5
Ryan Basque <ryan.basque.maitha@trademaster.com.br>
2021-11-10 22:35:22 -0300

    feat: headerBar links

--- src/components/atoms/HeaderBar/index.js
+++ src/components/atoms/HeaderBar/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { StyledHeaderBar } from './styles';
 
 const HeaderBar = () => {
   return (
-    <StyledHeaderBar>
-      
+    <StyledHeaderBar data-aos="flip-left">
+      <Link to="/">Entrar</Link>
     </StyledHeaderBar>
   );
 };
--- src/components/atoms/HeaderBar/styles.js
+++ src/components/atoms/HeaderBar/styles.js
@@ -1,6 +1,21 @@
 
 import styled from 'styled-components';
+import { color } from './../../../assets/css/colors';
 
 export const StyledHeaderBar = styled.header`
-  margin-bottom: 2rem;
+  padding: 1rem 2rem 1rem;
+  margin-bottom: 3rem;
+
+  width: 100%;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.1);
+
+  a {
+    text-decoration: none;
+    color: ${color.black};
+  }
 `;
